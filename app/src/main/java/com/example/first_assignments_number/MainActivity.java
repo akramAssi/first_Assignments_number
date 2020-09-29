6378b715f2853d7d32949d3e63f2094a9dcee557
akramassi <akram.20002010@gmail.com>
2020-09-29 23:37:52 +0300

    create function isPrime , isTwisted ,isPalindrome and startAninmated and Prepare the addTextChangedListener function

--- app/src/main/java/com/example/first_assignments_number/MainActivity.java
+++ app/src/main/java/com/example/first_assignments_number/MainActivity.java
@@ -3,34 +3,158 @@ package com.example.first_assignments_number;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
-import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView done;
-    AnimatedVectorDrawable d1;
-    AnimatedVectorDrawableCompat d2;
+    private TextView number;
+    private LinearLayout palindrome_background;
+    private LinearLayout prime_background;
+    private LinearLayout twisted_background;
+    private ImageView palindrome_view;
+    private ImageView prime_view;
+    private ImageView twisted_view;
+
+    private AnimatedVectorDrawable versionGreaterThen_23;
+    private AnimatedVectorDrawableCompat versionLessThan_24;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        done = findViewById(R.id.cor1);
-        Drawable ddo = done.getDrawable();
 
+        palindrome_background = findViewById(R.id.palindrome_background);
+        prime_background = findViewById(R.id.prime_background);
+        twisted_background = findViewById(R.id.twisted_background);
+        palindrome_view = findViewById(R.id.palindrome_view);
+        prime_view = findViewById(R.id.prime_view);
+        twisted_view = findViewById(R.id.twisted_view);
+        number = (EditText)findViewById(R.id.number);
+
+        number.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(number.getText().toString().isEmpty())
+                {
+                    palindrome_background.setBackgroundResource(0);
+                    prime_background.setBackgroundResource(0);
+                    twisted_background.setBackgroundResource(0);
+                    palindrome_view.setImageResource(R.drawable.no_value);
+                    prime_view.setImageResource(R.drawable.no_value);
+                    twisted_view.setImageResource(R.drawable.no_value);
+                } else  {
+
+
+                    twisted_background.setBackgroundResource(R.drawable.correct_circle);
+
+                    twisted_view.setImageResource(R.drawable.avd_done);
+
+                    if(isPalindrome(number.getText().toString()))
+                    {
+                        palindrome_background.setBackgroundResource(R.drawable.correct_circle);
+                        palindrome_view.setImageResource(R.drawable.avd_done);
+                    }
+                    else
+                    {
+                        palindrome_background.setBackgroundResource(R.drawable.incorrect_circle);
+                        palindrome_view.setImageResource(R.drawable.avd_false);
+                    }
+                    startAnimated(palindrome_view);
+
+                    if(isPrime(number.getText().toString()))
+                    {
+                        prime_background.setBackgroundResource(R.drawable.correct_circle);
+                        prime_view.setImageResource(R.drawable.avd_done);
+                    }
+                    else
+                    {
+                        prime_background.setBackgroundResource(R.drawable.incorrect_circle);
+                        prime_view.setImageResource(R.drawable.avd_false);
+                    }
+                    startAnimated(prime_view);
+
+                    if(isTwisted(number.getText().toString()))
+                    {
+                        twisted_background.setBackgroundResource(R.drawable.correct_circle);
+                        twisted_view.setImageResource(R.drawable.avd_done);
+                    }
+                    else
+                    {
+                        twisted_background.setBackgroundResource(R.drawable.incorrect_circle);
+                        twisted_view.setImageResource(R.drawable.avd_false);
+                    }
+                    startAnimated(twisted_view);
+
+
+                }
+            }
+        });
+
+    }
+
+    private void startAnimated(ImageView view)
+    {
+        Drawable ddo = view.getDrawable();
         if (ddo instanceof AnimatedVectorDrawableCompat) {
-            d2 = (AnimatedVectorDrawableCompat) ddo;
-            d2.start();
-            System.out.println("d2");
+            versionLessThan_24 = (AnimatedVectorDrawableCompat) ddo;
+            versionLessThan_24.start();
         } else if (ddo instanceof AnimatedVectorDrawable) {
-            d1 = (AnimatedVectorDrawable) ddo;
-            d1.start();
-            System.out.println("d1");
+            versionGreaterThen_23 = (AnimatedVectorDrawable) ddo;
+            versionGreaterThen_23.start();
+        }///end else if
+    }
+
+    private boolean isPalindrome(String string)
+    {
+        StringBuilder sb=new StringBuilder(string);
+        sb.reverse();
+
+        return string.equals(sb.toString());
+    }
+
+    private boolean isPrime(String string)
+    {
+        long num = Long.parseLong(string);
+        if (num < 2)
+            return false;
+
+
+        for(int i=2 ; i < num ; i++){
+
+            if ( num % i == 0 )
+                return false;
 
         }
+            return true;
     }
+
+    private boolean isTwisted(String string)
+    {
+        StringBuilder sb=new StringBuilder(string);
+        sb.reverse();
+        return (isPrime(sb.toString()) && isPrime(string));
+    }
+
+
 
 }
