99b9402bdfecfdca83cde5a2fa756881b322e450
akramassi <akram.20002010@gmail.com>
2020-09-30 21:35:32 +0300

    override onSupportNavigateUp and set onClike to text view

---
+++ app/src/main/java/com/example/first_assignments_number/about.java
@@ -0,0 +1,46 @@
+package com.example.first_assignments_number;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class about extends AppCompatActivity {
+
+    private Button bott;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ActionBar bar =getSupportActionBar();
+//        bar.setHomeButtonEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(true);
+
+
+
+        TextView linkTextView = findViewById(R.id.textView6);
+        linkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/akramAssi/first_Assignments_number"));
+                startActivity(i);
+
+            }
+        });
+
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
